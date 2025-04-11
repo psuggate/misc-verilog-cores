856ade2728870367403c3c6fda9d150a61bbaa3d
Patrick Suggate <patrick.suggate@gmail.com>
2025-04-11 15:34:38 +1200

    Tweaks.

--- rtl/misc/shift_register.v
+++ rtl/misc/shift_register.v
@@ -1,23 +1,17 @@
 `timescale 1ns / 100ps
-module shift_register (
-    clock,
-    wren_i,
-    addr_i,
-    data_i,
-    data_o
+module shift_register #(
+    parameter  WIDTH = 8,
+    localparam MSB   = WIDTH - 1,
+
+    parameter  DEPTH = 16,
+    localparam ASB   = $clog2(DEPTH) - 1
+) (
+    input clock,
+    input wren_i,
+    input [ASB:0] addr_i,
+    input [MSB:0] data_i,
+    output [MSB:0] data_o
 );
-
-  parameter WIDTH = 8;
-  localparam MSB = WIDTH - 1;
-
-  parameter DEPTH = 16;
-  localparam ASB = $clog2(DEPTH) - 1;
-
-  input clock;
-  input wren_i;
-  input [ASB:0] addr_i;
-  input [MSB:0] data_i;
-  output [MSB:0] data_o;
 
   reg [MSB:0] data_q;
 
@@ -25,28 +19,23 @@ module shift_register (
 
   genvar ii;
   generate
-    // begin : g_shift
+    reg [MSB:0] srl[0:DEPTH-1];
 
-      // begin : g_shift_register
-      reg [MSB:0] srl[0:DEPTH-1];
-
-      for (ii = 0; ii < DEPTH - 1; ii = ii + 1) begin : g_forshi
-        always @(posedge clock) begin
-          if (wren_i) begin
-            srl[ii+1] <= srl[ii];
-          end
-        end
-      end
-
+    for (ii = 0; ii < DEPTH - 1; ii = ii + 1) begin : g_forshi
       always @(posedge clock) begin
         if (wren_i) begin
-          srl[0] <= data_i;
+          srl[ii+1] <= srl[ii];
         end
+      end
+    end
 
-        data_q <= srl[addr_i];
+    always @(posedge clock) begin
+      if (wren_i) begin
+        srl[0] <= data_i;
       end
 
-    // end
+      data_q <= srl[addr_i];
+    end
   endgenerate  // g_shift_register
 
 endmodule  // shift_register
